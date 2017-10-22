88fbf974abf2bcec209d15c5fd8bfb94d780277b
rictorlome <33004639+rictorlome@users.noreply.github.com>
2017-10-22 16:57:13 -0400

    Update Mario Less.c

--- Mario Less.c
+++ Mario Less.c
@@ -9,12 +9,17 @@ int main(void)
     int h = get_int();
     if (h < 1 || h > 23) {
         printf("Please pick an integer between 1 and 23\n");
-    } else {hash_row(h);}
+    } else {
+        for (int k = 0; k < h; k++) {
+        hash_row(k);
+        printf("\n");
+        }
+    }
 
 }
 void hash_row(int n)
 {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n + 1; i++) {
         printf("#");
     }
 }
