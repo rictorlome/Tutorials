ba74361c6b4d51cff331442e2817562d45404262
rictorlome <33004639+rictorlome@users.noreply.github.com>
2017-10-22 18:05:40 -0400

    Update Mario Less.c
    Not working because number of spaces increases while number of blocks increases. Number of spaces should start at (height - 1) and decrease down to zero, to align the bottom left of the half-pyramid with the left side of the terminal.

--- Mario Less.c
+++ Mario Less.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 void hash_row(int n);
-
+void space_row (int n);
 int main(void)
 {
     printf("What is the half-pyramid's height?");
     int h = get_int();
-    if (h < 1 || h > 23) {
-        printf("Please pick an integer between 1 and 23\n");
+    if (h < 0 || h > 23) {
+        printf("Please pick an integer between 0 and 23\n");
     } else {
         for (int k = 0; k < h; k++) {
+        space_row(k);
         hash_row(k);
         printf("\n");
         }
@@ -21,5 +22,11 @@ void hash_row(int n)
 {
     for (int i = 0; i <= n + 1; i++) {
         printf("#");
+    }
+}
+void space_row (int n)
+{
+    for (int j = n - 1; j > 0 ; j--) {
+        printf(" ");
     }
 }
