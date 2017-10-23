ed1922b4548c6ab483ac2bf9b9392e2ea2f3a47a
rictorlome <33004639+rictorlome@users.noreply.github.com>
2017-10-23 09:45:23 -0400

    Update Mario Less.c
    I think it works.

--- Mario Less.c
+++ Mario Less.c
@@ -1,32 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
-
-void hash_row(int n);
-void space_row (int n);
+void repeat(int n, string X);
 int main(void)
 {
-    printf("What is the half-pyramid's height?");
+    printf("What is the half-pyramid's height?\n");
     int h = get_int();
     if (h < 0 || h > 23) {
         printf("Please pick an integer between 0 and 23\n");
     } else {
         for (int k = 0; k < h; k++) {
-        space_row(k);
-        hash_row(k);
-        printf("\n");
+            repeat(-k + h - 1, " "); // Prints 'space' height - (1 + k) times
+            repeat(k + 2, "#");      // Prints '#' k + 2 times
+            printf("\n");
         }
-    }
+            }
+}
 
-}
-void hash_row(int n)
+void repeat(int n, string X) // Given a number 'n' and a char 'X', repeatedly print the 'item' 'n' times.
 {
-    for (int i = 0; i <= n + 1; i++) {
-        printf("#");
-    }
-}
-void space_row (int n)
-{
-    for (int j = n - 1; j > 0 ; j--) {
-        printf(" ");
+    for (int i = 0; i < n; i++) {
+        printf("%s", X);
     }
 }
