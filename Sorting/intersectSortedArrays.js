607af0c7d0fa3d1f4d5b1c2de7ce1851be6079b9
rictorlome <33004639+rictorlome@users.noreply.github.com>
2017-11-18 18:58:59 -0500

    Update intersectSortedArrays.js

--- Sorting/intersectSortedArrays.js
+++ Sorting/intersectSortedArrays.js
@@ -1,17 +1,24 @@
-//inputs sorted arrays, outputs an array of their intersection.
-//will duplicate repeats if they appear in the input arrays.
-//works a lot like mergeSort, using array pointers and changing the pointer value based on which condition is satisfied.
+//updated version of intersectSortedArrays that repeats shouldn't break
+//the way it avoids repeats is by keeping track of the lastpush, and then incrementing p1 or p2 if arr1[p1] or arr2[p2] matches the last pushed
 
 function intersectSortedArrays (arr1,arr2) {
   var n = arr1.length;
   var m = arr2.length
   var p1 = 0;
   var p2 = 0;
+  var lastPushed;
   var intersectSorted = [];
   //the loop will terminate when either of the arrays has been iterated through (there won't be any more matches)
-  //if there's a match, increase the 
   while (p1 < n && p2 < m) {
-    if (arr1[p1] === arr2[p2]) {
+    if (arr1[p1] === lastPushed) {
+      p1++;
+    }
+    if (arr2[p2] === lastPushed) {
+      p2++;
+    }
+    //i only have it checking whether one of the current values is !== lastPushed because they have to be equal to push, and if 1 is equal to the lastPush, the other one will be too.
+    if (arr1[p1] === arr2[p2] && arr1[p1] !== lastPushed) {
+      lastPushed = arr1[p1];
       intersectSorted.push(arr1[p1]);
       p1++;
       p2++;
